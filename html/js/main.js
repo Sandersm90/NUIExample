40125c757feae0d59b3f976a63205ed3d3361c99
Michael S <siniq90@gmail.com>
2018-06-13 11:57:38 +0200

    Update main.js
    strings should be in single-quotes

--- html/js/main.js
+++ html/js/main.js
@@ -9,17 +9,17 @@ window.addEventListener('message', (event) => {
 });
 
 function HandleExample(item) {
-    $("#sendHello").show()
+    $('#sendHello').show()
     // showing the incoming data in the span
     $('#helloworldShower').text(item.examplemessage1)
 }
 
 $(document).ready(function() {
-    $("#sendHello").click(function () {
+    $('#sendHello').click(function () {
         console.log('btn clicked')
         $.ajax({
-            url: "http://exnui/helloworld",
-            data: JSON.stringify({ name: "John", lastname: "Doe" }),
+            url: 'http://exnui/helloworld',
+            data: JSON.stringify({ name: 'John', lastname: 'Doe' }),
             type: 'post'
         })
             // ran when the ajax is done
